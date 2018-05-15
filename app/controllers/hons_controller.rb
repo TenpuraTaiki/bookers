f11a5503dafcf695c421e8efeebe1d99362d478e
TenpuraTaiki <sleeping.3.fairies@gmail.com>
2018-05-15 16:44:51 +0900

    とりあえず初提出するのが完成

--- app/controllers/hons_controller.rb
+++ app/controllers/hons_controller.rb
@@ -1,10 +1,41 @@
 class HonsController < ApplicationController
   def index
+    @hons = Hon.all
+    @hon = Hon.new
   end
 
   def show
+    @hon = Hon.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    hon = Hon.new(hon_params)
+    hon.save
+
+    redirect_to hons_path
   end
 
   def edit
+    @hon = Hon.find(params[:id])
   end
+
+  def update
+    hon = Hon.find(params[:id])
+    hon.update(hon_params)
+    redirect_to hon_path(hon)
+  end
+
+  def destroy
+    hon =Hon.find(params[:id])
+    hon.destroy
+    redirect_to hons_path
+  end
+
+  private
+    def hon_params
+      params.require(:hon).permit(:title, :body)
+    end
 end
